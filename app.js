26a9d680cec2dde0173c5bf2772c61d381410628
abdulwahab-github <112512244+abdulwahab-github@users.noreply.github.com>
2022-09-15 22:01:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,248 @@
+// var a = ("wahab"); 
+//  var b = a.toUpperCase();
+//  console.log(b);
+
+
+// var a = ("wahab"); 
+//  var b = a.tolowerCase();
+//  console.log(b);
+
+// var a = prompt("enter your fvrt mobile");
+// var b = a.length;
+// alert (b);
+
+// var a = ("welcome man");
+// var b = a.slice(8,11);
+// alert(b);
+// var a = ("pakistani");
+// var b = a.indexOf("n");
+// alert(b);
+// var a = prompt("enter your name");
+// var b = ("etc", "!");
+// if(a == b ){
+//     alert("");
+// }else{
+//     alert("enter a valid username")
+// }
+// var a = ("A quick bowler thro a bowl to the ground");
+//  var b = a.indexOf("the");
+//  alert(b);
+// var a = ("pakistani");
+// var b = a.slice(3,4);
+// alert(b);
+// var a = "Hyderabad";
+// var b = a.replace("abad", "Islam");
+// alert (b );
+// var a = ("ali and shami play crickert and football");
+// var b = a.replace ("and","&");
+// // alert(b);
+// var a = Math.round(3.5652);
+// alert(a);
+// var a = Math.ceil(3.2652);
+// alert(a);
+// var a = Math.floor(3.5652);
+// alert(a);
+// var a = ("472");
+// var b = Number(a);
+// alert(typeof( b));
+// var a = 35.36;
+// var b = a.toString();
+// alert(typeof(b));
+// var a = 30;
+// var b = 45;
+// var c = a/b;
+// var todayDate = new Date("09-30-2022");
+// var a = todayDate.toString();
+// var b = a.slice(8,10)
+
+// if (b == "1"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+
+//  else if (b == "2"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "3"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "3"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "4"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "5"){
+//     alert("First Fiifteen Days Of the Month")
+// }else if (b == "6"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "7"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "8"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "9"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "10"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "11"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "12"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "13"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "14"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "15"){
+//     alert("First Fiifteen Days Of the Month")
+// }
+//  else if (b == "16"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "17"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "18"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "19"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "20"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "21"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "22"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "23"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "25"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "26"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "27"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "28"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "29"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "30"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+//  else if (b == "31"){
+//     alert("Last Fiifteen Days Of the Month")
+// }
+
+
+
+// // alert(c.toFixed(2));
+
+// // var a = new Date();
+// // document.write(a);
+// // var a= new Date();
+
+// // document.write(a.getDay())
+// // // var b = ("today is ")
+// // // var a = ("sunday")
+// // //  var b = a.slice(0 , 2);
+
+// // // alert(a);
+// //  var a = ("saturday", "sunday");
+// //  if(a = a){
+// //     alert("its a fun day")
+
+// //  }
+// // var a = new Date(20-9-2022);
+// //   var b = a.getDate()
+// //   if( a  < 15){
+
+// //     alert("fist fiteen days")
+// //   }else if(a > 15){
+// //             alert("last day")    
+// //   }
+// // var b = new Date(1-1-1970);
+// // var e = b.getMilliseconds();
+// // var a = new Date();
+// // var d = a.getMilliseconds();
+
+// // alert(a * b );
+// // var a = new Date(1-1-1970)
+// // var b = a.getMinutes()
+
+// // // var c = new Date()
+// // // var d = c.getMinutes()
+// // alert(b);
+// //    var a = ("Abdul wahab")
+// //    var b = new Date("January 01 2022");
+// //    var c = b.setMonth('february');
+// //    var d = ('410');
+// //    var e = ('20');
+// //    var f = ("6502")
+// //    var g = (450);
+// //    var D = ("6300");
+// //    var x = (d * e );
+
+// //    document.write("customer Name " + a  + "<br>") 
+// //    document.write('month: ' +  b.toDateString()+"<br>");
+// //    document.write("units: "  + d +"<br>");
+// //    document.write(" charges per units : " + e +"<br>");
+// //    document.write( "<br>" + "NET AMOUNT PAYABLE  (WITHIN DUE DATE): " + d * e ) ;
+// //    document.write( "<br>" + "GROSS AMOUNT PAYABLE (AFTER DUE DATE) " + (x + g )  );
+//    var a = new Date();
+//    document.write(a);
+// function  abc() {
+//     document.write("Wahab "+ " qadir")
+    
+// }
+// abc("Wahab ","qadir")
+// var a = prompt("enter any odd number");
+// var b = prompt("enter any even number");
+ 
+// function sum(a,b) {
+//     var c = (a * b)
+    
+//     document.write(c)
+    
+// }
+// sum(a , b)
+// var a = "the quickbrown fox"
+// function LETTER(){    
+// var b = a.toUpperCase();
+// document.write(b);
+// }
+// LETTER("the quickbrown fox")
+
+
+
+
+// var a = 'web development tutorials'
+// function letter() {
+//     var b = a.slice(3 , 16);
+// document.write(b);
+    
+// }
+// letter('web development tutorials');
+
+// var calcCircumference = prompt("enter Circumference");
+// function radius(){
+//     var a = (calcCircumference * radius)
+//     document.write(a)
+
+// }
+// radius(20)
